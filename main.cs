0f49fe35e7713f5a64e94af03de23d01c1602253
Khondker Montasirzzaman <kmontasi@jyu.fi>
2021-10-04 15:14:19 +0300

    Update 4-20-21 15.14

--- main.cs
+++ main.cs
@@ -6,18 +6,36 @@ using Jypeli.Widgets;
 using System;
 using System.Collections.Generic;
 
-    public class pallopeli : PhysicsGame
-{
+    public class main : PhysicsGame
+    {
+        public override void begin()
+        {
+            TasoTutoriaali();
+
+        }
 
 
+    
 
-}
-
-
+    public override void TasoTutoriaali()
+    {
+    protected override void Paint(Canvas canvas)
+    {
+        canvas.BrushColor = Color.Red;
+        double x = canvas.Left + 100, y = canvas.Top - 100;
+        canvas.DrawLine(new Vector(x - 50, y + 50), new Vector(x + 50, y - 50));
+        canvas.DrawLine(new Vector(x + 50, y + 50), new Vector(x - 50, y - 50));
+        base.Paint(canvas);
+    }
+    }
 
 
     protected override void Paint(Canvas canvas)
 {
-    // T‰ss‰ v‰liss‰ piirret‰‰n kuviot
+    canvas.BrushColor = Color.Red;
+    double x = canvas.Left + 100, y = canvas.Top - 100;
+    canvas.DrawLine(new Vector(x - 50, y + 50), new Vector(x + 50, y - 50));
+    canvas.DrawLine(new Vector(x + 50, y + 50), new Vector(x - 50, y - 50));
     base.Paint(canvas);
 }
+    }
