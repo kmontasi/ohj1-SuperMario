b3f3d8a7d4e7e91ad7206a091324e6387b7da0d0
Khondker Montasirzzaman <kmontasi@jyu.fi>
2021-10-04 14:12:38 +0300

    Päivitys

--- main.cs
+++ main.cs
@@ -1,1 +1,23 @@
-test alpha 4-10-21
+//test alpha 4-10-21//
+using Jypeli;
+using Jypeli.Assets;
+using Jypeli.Controls;
+using Jypeli.Widgets;
+using System;
+using System.Collections.Generic;
+
+    public class pallopeli : PhysicsGame
+{
+
+
+
+}
+
+
+
+
+    protected override void Paint(Canvas canvas)
+{
+    // T‰ss‰ v‰liss‰ piirret‰‰n kuviot
+    base.Paint(canvas);
+}
